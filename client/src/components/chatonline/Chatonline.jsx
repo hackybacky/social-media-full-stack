ac07efbd6958f3744c4322640a732ab3274976b1
hackybacky <hacky3244@gmail.com>
2022-03-29 16:39:44 +0530

    commit

---
+++ client/src/components/chatonline/Chatonline.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './chatonline.css'
+export default function Chatonline() {
+  return (
+    <div className="chatOnline">
+      <div className="chatOnlineFriend">
+        <div className="chatOnlineImgContainer">
+          <img src="https://static.onecms.io/wp-content/uploads/sites/6/2020/05/07/MSDVAGI_OR006.jpg" alt="" className="chatOnlineImg" />
+          <div className="chatOnlineBadge"></div>
+        </div>
+        <div className="chatOnlineName">
+          john
+        </div>
+      </div>
+    </div>
+  )
+}
--- client/src/pages/messenger/Messenger.jsx
+++ client/src/pages/messenger/Messenger.jsx
@@ -3,38 +3,51 @@ import Topbar from "../../components/topbar/Topbar";
 import "./messenger.css";
 import Conversation from "../../components/conversations/Conversation";
 import Message from "../../components/message/Message";
+import Chatonline from "../../components/chatonline/Chatonline";
 export default function Messenger() {
   return (
     <>
       <Topbar />
       <div className="messenger">
-          <div className="chatMenu">
-              <div className="chatMenuWrapper">
-                  <input placeholder="search for friends" className="chatMenuInput" />
-                  <Conversation/>
-                  <Conversation/>
-                  <Conversation/>
-                  <Conversation/>  
-
-              </div>
+        <div className="chatMenu">
+          <div className="chatMenuWrapper">
+            <input placeholder="search for friends" className="chatMenuInput" />
+            <Conversation />
+            <Conversation />
+            <Conversation />
+            <Conversation />
           </div>
-          <div className="chatBox">
-              <div className="chatBoxWrapper">
-                  <div className="chatBoxTop">
-                    <Message/>
-                    {/* <Message own={true}/> */}
-                    <Message/>
-                   
-                  </div>
-                  <div className="chatBoxBottom">
-                      <textarea name="" id="" cols="30" rows="10" placeholder="write something" className="chatMessageInput"></textarea>
-                      <button className="chatSubmitbutton">send</button>
-                  </div>
-              </div>
+        </div>
+        <div className="chatBox">
+          <div className="chatBoxWrapper">
+            <div className="chatBoxTop">
+              <Message />
+              <Message own={true} />
+              <Message />
+              <Message />
+              <Message />
+              <Message />
+              <Message />
+              <Message />
+              <Message />
+              <Message />
+              <Message />
+              <Message />
+            </div>
+            <div className="chatBoxBottom">
+              <textarea
+                placeholder="write something"
+                className="chatMessageInput"
+              ></textarea>
+              <button className="chatSubmitButton">send</button>
+            </div>
           </div>
-          <div className="chatOnline">
-              <div className="chatOnlineWrapper">online chat </div>
+        </div>
+        <div className="chatOnline">
+          <div className="chatOnlineWrapper">
+            <Chatonline />
           </div>
+        </div>
       </div>
     </>
   );
